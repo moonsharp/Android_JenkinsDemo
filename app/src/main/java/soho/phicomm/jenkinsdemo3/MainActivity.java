972fa594738531f7ea6186f7f97e4775a023ec65
qisheng.lv <qisheng.lv@phicomm.com>
2017-03-29 13:51:11 +0800

    init project

---
+++ app/src/main/java/soho/phicomm/jenkinsdemo3/MainActivity.java
@@ -0,0 +1,26 @@
+package soho.phicomm.jenkinsdemo3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_hello)
+    public void tv_hello(){
+        Toast.makeText(this, "hello jenkins", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
